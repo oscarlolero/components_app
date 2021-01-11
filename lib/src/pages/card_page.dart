2a06361cbd730db49990e5ff9e0177a88cdea23c
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-11 16:12:44 -0600

    Card page

---
+++ lib/src/pages/card_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CardPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
+        children: <Widget>[
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+          _cardTipo1(),
+          SizedBox(height: 30.0),
+          _cardTipo2(),
+          SizedBox(height: 30.0),
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo1() {
+    return Card(
+      elevation: 10,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      child: Column(
+        children: [
+          ListTile(
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
+            title: Text('titulo'),
+            subtitle: Text(
+                'hola buenas tardes aksdjsakjd e aksjdksa asdkjads askjdakjasd'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              FlatButton(onPressed: () {}, child: Text('Cancelar')),
+              FlatButton(onPressed: () {}, child: Text('Ok')),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardTipo2() {
+    final card = Container(
+      // clipBehavior: Clip.antiAlias,
+      child: Column(
+        children: [
+          FadeInImage(
+            placeholder: AssetImage('assets/loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 300,
+            fit: BoxFit.cover,
+            image: NetworkImage(
+                'https://alphauniverseglobal.media.zestyio.com/banff-johnson-lake-sunset-james-brandon.jpg'),
+          ),
+          // Image(
+          //     image: NetworkImage(
+          //         'https://alphauniverseglobal.media.zestyio.com/banff-johnson-lake-sunset-james-brandon.jpg')),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('asdsa'),
+          ),
+        ],
+      ),
+    );
+
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black26,
+            blurRadius: 10.0,
+            spreadRadius: 2.0,
+            offset: Offset(2,10)
+          )
+        ]
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: card,
+      ),
+    );
+  }
+}
