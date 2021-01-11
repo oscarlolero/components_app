d3f2078d82a6ce266b2bb2dfd539ad23d8694988
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-11 16:37:09 -0600

    Alert page

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,12 +7,48 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('alert page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar alerta'),
+          onPressed: () => _showAlert(context),
+          textColor: Colors.white,
+          color: Colors.blue,
+          shape: StadiumBorder(),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.keyboard_return),
         onPressed: () {
           Navigator.pop(context);
         },
       ),
+    );
+  }
+
+  void _showAlert(BuildContext context) {
+    showDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return AlertDialog(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          title: Text('titulo'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('asdasd'),
+              FlutterLogo(size: 100,)
+            ],
+          ),
+
+          actions: [
+            FlatButton(onPressed: () => Navigator.of(context).pop(), child: Text('Cancelar')),
+            FlatButton(onPressed: () {
+              Navigator.of(context).pop();
+            }, child: Text('Ok')),
+          ],
+        );
+      }
     );
   }
 }
