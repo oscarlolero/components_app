23b7a69b864bdf23966925e9903d17e37db9252d
Oscar Montes <oscmcojc1@live.com.mx>
2021-01-11 16:44:54 -0600

    Avatar page

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,12 +1,38 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('avatar page'),
-        )
+      appBar: AppBar(
+        title: Text('avatar page'),
+        actions: [
+          Container(
+            padding: EdgeInsets.all(5),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://ichef.bbci.co.uk/news/640/cpsprodpb/126F3/production/_95870557_gettyimages-521230034.jpg'),
+              radius: 25,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          fadeInDuration: Duration(milliseconds: 200),
+          placeholder: AssetImage('assets/loading.gif'),
+          image: NetworkImage(
+              'https://ichef.bbci.co.uk/news/640/cpsprodpb/126F3/production/_95870557_gettyimages-521230034.jpg'),
+        ),
+      ),
     );
   }
 }
